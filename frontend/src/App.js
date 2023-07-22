81d492ae13c76e6df5e169e4686079cbeeef468d
Naranja-Sagged <alexliuying@gmail.com>
2023-07-22 00:46:57 -0700

    Lobbies start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,61 +2,102 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-const lobbies = [
-  {
-    id: 1,
-  },
-  {
-    id: 2,
-  },
-  {
-    id: 3,
-  },
-  {
-    id: 4,
-  },
-];
-
 function App() {
-  const [name, setName] = useState(undefined);
+  const [name, setName] = useState("");
   const [phase, setPhase] = useState("NAME_PHASE");
+  const [lobbies, setLobbies] = useState([]);
 
-  if (phase === "NAME") {
+  if (phase === "NAME_PHASE") {
     // render something
+    return (
+      <div className="skr-name">
+        <p>hey you need to enter a name broski</p>
+        <NameInput name={name} setName={setName} setPhase={setPhase} />
+      </div>
+    );
+  } else if (phase === "OUT_LOBBY") {
+    // render something else
+    return (
+      <Lobbies name={name} lobbies={lobbies} setLobbies={setLobbies}></Lobbies>
+    );
   } else if (phase === "IN_LOBBY") {
     // render something else
   } else if (phase === "GAME") {
     // render something else
   }
-
-  if (!name) {
-    return (
-      <div className="skr-name">
-        <p>hey you need to enter a name broski</p>
-        <Input name={name} setName={setName} />
-      </div>
-    );
-  } else {
-    return <Lobbies></Lobbies>;
-  }
 }
 
-function Input(props) {
+function NameInput(props) {
+  const [value, setValue] = useState(undefined);
+
   return (
-    <input
-      value={props.name}
-      onInput={(event) => {
-        props.setName(event.target.value);
-      }}
-    ></input>
+    <>
+      <input
+        value={value}
+        onInput={(event) => {
+          setValue(event.target.value);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          props.setName(value);
+          props.setPhase("OUT_LOBBY");
+        }}
+      >
+        Ok
+      </button>
+    </>
   );
 }
 
 function Lobbies(props) {
+  const [newLobbyName, setNewLobbyName] = useState("");
+  const [newLobbyPassword, setNewLobbyPassword] = useState("");
+
   console.log("props", props);
-  return lobbies.map((lobby) => (
-    <div key={lobby.id}>This is my lobby: {lobby.id}</div>
-  ));
+  return (
+    <>
+      <div>You're Username: {props.name}</div>
+      {props.lobbies.map((lobby) => (
+        <div key={lobby.lobbyname}>
+          Lobby {lobby.lobbyname}{" "}
+          <button onClick={(e) => {}}>Join Lobby</button>
+          {lobby.password && (
+            <>
+              <label>Enter Password</label>
+              <input></input>
+            </>
+          )}
+        </div>
+      ))}
+      New lobby name:{" "}
+      <input
+        value={newLobbyName}
+        onInput={(e) => {
+          setNewLobbyName(e.target.value);
+        }}
+      ></input>
+      New lobby password:{" "}
+      <input
+        value={newLobbyPassword}
+        onInput={(e) => {
+          setNewLobbyPassword(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={(e) => {
+          props.setLobbies(
+            props.lobbies.concat({
+              lobbyname: newLobbyName,
+              password: newLobbyPassword,
+            })
+          );
+        }}
+      >
+        Create Lobby!
+      </button>
+    </>
+  );
 }
 
 export default App;
