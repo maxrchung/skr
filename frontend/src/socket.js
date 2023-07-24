cf5707afa116342f0d91e9da0c6c5c0a7c6c4e99
maxrchung <maxrchung@gmail.com>
2023-07-23 20:03:53 -0700

    quick hack for the right prod url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -4,6 +4,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const url =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
+  process.env.NODE_ENV === "production"
+    ? window.location.host.replace(":3000", ":4000")
+    : "http://localhost:4000";
 
 export const socket = io(url);
